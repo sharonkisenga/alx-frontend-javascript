8faccc6880f928aa2963cea9aca7a6e9365e4f90
sharonkisenga <sharonkisenga>
2024-02-05 06:06:46 +0000

    javascriptbasics

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+xport default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
+}
