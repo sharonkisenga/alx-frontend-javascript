ff96d9276cf1004af71c61ff427688baec69a9be
sharonkisenga <sharonkisenga@gmail.com>
2024-02-13 09:09:54 +0000

    manupulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrade){
+  return students
+    .filter((student) => student.location == city)
+    .map((student) => {
+      let grades = newGrade.find((grade) => grade.studentId == student.id);
+      let grade = grades ? grades.grade : "N/A";
+      return { ...student, grade };
+     });
+}
